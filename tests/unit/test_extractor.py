f975d573c80ed51260a74406e800c77d73d54319
Mathias Lohne <mathias.lohne@cognite.com>
2022-01-17 14:14:27 +0100

    Add ability to do POST requests (#7)
    Add a `post` decorator to the RestExtractor class. Use Python lists and
    dicts to create body templates, with values either being given directly
    or through callables, for example:
    
    ``` python
    @extractor.post(
        "/path",
        response_type=SomeClass,
        body={
            "key1": "value1",
            "key2": lambda: [1, 2, 3],
            some_variable: some_function
        }
    )
    def handle(...):
    ```

---
+++ tests/unit/test_extractor.py
@@ -0,0 +1,78 @@
+#  Copyright 2022 Cognite AS
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import unittest
+
+from cognite.extractorutils.rest.extractor import _format_body, _get_or_call
+
+
+class Iterator:
+    def __init__(self) -> None:
+        self.num = 0
+
+    def __call__(self) -> int:
+        self.num += 1
+        return self.num
+
+
+class TestFormatBody(unittest.TestCase):
+    def test_empty_body(self) -> None:
+        self.assertIsNone(_format_body(None))
+
+    def test_dict_values(self) -> None:
+        self.assertEqual(
+            _format_body({"key1": "val", "key2": 42, "key3": None}), '{"key1": "val", "key2": 42, "key3": null}'
+        )
+
+    def test_recursive_dict_values(self) -> None:
+        self.assertEqual(
+            _format_body({"key1": "val", "key2": {"subkey": "subval"}, "key3": ["item1", "item2", "item3"]}),
+            '{"key1": "val", "key2": {"subkey": "subval"}, "key3": ["item1", "item2", "item3"]}',
+        )
+
+    def test_callable(self) -> None:
+        self.assertEqual(_format_body({"key1": lambda: "val"}), '{"key1": "val"}')
+
+    def test_recursive_callable(self) -> None:
+        self.assertEqual(_format_body({"key1": lambda: {"subkey": lambda: "subval"}}), '{"key1": {"subkey": "subval"}}')
+
+    def test_callable_in_list(self) -> None:
+        self.assertEqual(_format_body({"key": lambda: ["item1", lambda: "item2"]}), '{"key": ["item1", "item2"]}')
+
+    def test_empty_object(self) -> None:
+        self.assertEqual(_format_body({}), "{}")
+        self.assertEqual(_format_body(lambda: {}), "{}")
+
+    def test_callable_class(self) -> None:
+        it = Iterator()
+        self.assertEqual(_format_body({"key": lambda: [it, it, it, it]}), '{"key": [1, 2, 3, 4]}')
+        self.assertEqual(_format_body({"key": lambda: [it, it, it, it]}), '{"key": [5, 6, 7, 8]}')
+
+
+class TestGetOrCall(unittest.TestCase):
+    def test_none(self) -> None:
+        self.assertIsNone(_get_or_call(None))
+
+    def test_value(self) -> None:
+        self.assertEqual(_get_or_call(42), 42)
+        self.assertEqual(_get_or_call("hey"), "hey")
+
+    def test_callable(self) -> None:
+        self.assertEqual(_get_or_call(lambda: 42), 42)
+        self.assertEqual(_get_or_call(lambda: 1 + 2 + 3), 6)
+        self.assertEqual(_get_or_call(lambda: "hey"), "hey")
+
+
+if __name__ == "__main__":
+    unittest.main()
